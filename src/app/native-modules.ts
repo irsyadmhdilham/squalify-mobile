7485be20aebdbbd33336f61da1c2f79578ef4deb
irsyadmhdilham <irsyadmhdilham@gmail.com>
2019-01-07 22:08:32 +0800

    update

--- src/app/native-modules.ts
+++ src/app/native-modules.ts
@@ -1,7 +1,6 @@
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Network } from "@ionic-native/network";
-import { ImagePicker } from "@ionic-native/image-picker";
 import { CallNumber } from "@ionic-native/call-number";
 import { Firebase } from "@ionic-native/firebase";
 import { Deeplinks } from "@ionic-native/deeplinks";
@@ -13,7 +12,6 @@ export const NativeModules = [
   StatusBar,
   SplashScreen,
   Network,
-  ImagePicker,
   CallNumber,
   Firebase,
   Deeplinks,
